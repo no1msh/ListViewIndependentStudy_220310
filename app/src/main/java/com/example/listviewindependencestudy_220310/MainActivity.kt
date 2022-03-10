a0e59ecc51fe2549a9f26894b6369641e98c3225
TFQ_Moon <no1msh1217@gmail.com>
2022-03-10 21:44:36 +0900

    아이템 클릭 리스너 설정과 실제 보여질 텍스트뷰와 데이터 연결
    - CharacterAdapter 실제 보여질 텍스트뷰와 데이터 연결
    - MainActivity 클릭리스너 추가

--- app/src/main/java/com/example/listviewindependencestudy_220310/MainActivity.kt
+++ app/src/main/java/com/example/listviewindependencestudy_220310/MainActivity.kt
@@ -2,37 +2,48 @@ package com.example.listviewindependencestudy_220310
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.listviewindependencestudy_220310.adapters.CharacterAdapter
 import com.example.listviewindependencestudy_220310.datas.CharacterData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val characterList = ArrayList<CharacterData>()
+    val mCharacterList = ArrayList<CharacterData>()
 
     lateinit var mAdapter : CharacterAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setEvent()
         setValue()
     }
 
+    fun setEvent(){
+        mainListView.setOnItemClickListener { parent, view, position, id ->
+
+            val ClickedCharacter = mCharacterList[position]
+
+            Toast.makeText(this, "${ClickedCharacter.name} is Clicked", Toast.LENGTH_SHORT).show()
+            
+        }
+    }
+    
     fun setValue(){
 
-        characterList.add( CharacterData("Tracer","DPS","UK"))
-        characterList.add( CharacterData("Reinhardt","Tank","Germany"))
-        characterList.add( CharacterData("Lucio","Support","Brazil"))
-        characterList.add( CharacterData("Soldier","DPS","USA"))
-        characterList.add( CharacterData("D.Va","Tank","South Korea"))
-        characterList.add( CharacterData("Mercy","Support","Swiss"))
-        characterList.add( CharacterData("Zenyatta","Support","Nepal"))
-        characterList.add( CharacterData("Brigitte","Support","Sweden"))
-        characterList.add( CharacterData("Cassidy","DPS","USA"))
-        characterList.add( CharacterData("Genzi","DPS","Japan"))
+        mCharacterList.add( CharacterData("Tracer","DPS","UK"))
+        mCharacterList.add( CharacterData("Reinhardt","Tank","Germany"))
+        mCharacterList.add( CharacterData("Lucio","Support","Brazil"))
+        mCharacterList.add( CharacterData("Soldier76","DPS","USA"))
+        mCharacterList.add( CharacterData("D.Va","Tank","South Korea"))
+        mCharacterList.add( CharacterData("Mercy","Support","Swiss"))
+        mCharacterList.add( CharacterData("Zenyatta","Support","Nepal"))
+        mCharacterList.add( CharacterData("Brigitte","Support","Sweden"))
+        mCharacterList.add( CharacterData("Cassidy","DPS","USA"))
+        mCharacterList.add( CharacterData("Genzi","DPS","Japan"))
 
-        mAdapter = CharacterAdapter(this, R.layout.characters_list_item , characterList)
+        mAdapter = CharacterAdapter(this, R.layout.characters_list_item , mCharacterList)
 
         mainListView.adapter = mAdapter
 
--- app/src/main/java/com/example/listviewindependencestudy_220310/adapters/CharacterAdapter.kt
+++ app/src/main/java/com/example/listviewindependencestudy_220310/adapters/CharacterAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.listviewindependencestudy_220310.R
 import com.example.listviewindependencestudy_220310.datas.CharacterData
 
@@ -19,12 +20,20 @@ class CharacterAdapter(
 
         var tempRow = convertView
         if (tempRow == null) {
-            tempRow = LayoutInflater.from(mContext).inflate(R.layout.characters_list_item,null)
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.characters_list_item, null)
         }
 
         val row = tempRow!!
 
+        val data = mList[position]
 
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val txtPosition = row.findViewById<TextView>(R.id.txtPosition)
+        val txtCountry = row.findViewById<TextView>(R.id.txtCountry)
+
+        txtName.text = data.name
+        txtPosition.text = data.heroPosition
+        txtCountry.text = data.country
 
         return row
 
