2fcfdc0300ff04f2bf162c11a420c3de7e34f290
TFQ_Moon <no1msh1217@gmail.com>
2022-03-15 13:48:03 +0900

    DataBinding 초기작업 구현

--- app/src/main/java/com/example/listviewindependencestudy_220310/MainActivity.kt
+++ app/src/main/java/com/example/listviewindependencestudy_220310/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.listviewindependencestudy_220310
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.listviewindependencestudy_220310.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
     }
+
 }
